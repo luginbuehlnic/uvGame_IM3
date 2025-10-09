1ea27f3f16691a529af95cf98115cbd6439912e0
Nic Luginbuehl <70602291+luginbuehlnic@users.noreply.github.com>
2025-10-09 15:43:34 +0200

    undload data and create chart

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const apiUrl = "https://uv-game.nic-luginbuehl.ch/unload.php"; // Passen Sie die URL bei Bedarf an
+
+    dataset = []
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            const ctx = document.getElementById("uviChart").getContext("2d");
+
+            dataSet = {
+                labels: data.map(item => item.location),
+                datasets: [{
+                    label: "UV Index",
+                    data: data.map(item => item.uvi),
+                    backgroundColor: [
+                    "#6B7280",
+                    "#93C5FD",
+                    "#A7F3D0",
+                    "#FDE68A",
+                    "#FCA5A5", 
+                    "#C4B5FD",
+                    "#F9A8D4",
+                    "#FDBA74",
+                    "#86EFAC",
+                    "#BFDBFE"
+                    ]
+                }]
+            };      
+
+            new Chart(ctx, { 
+                type: 'bar',
+                data: dataSet,
+                options: {
+                    grouped: false,
+                    indexAxis: 'y',
+                    scales: {
+                        x: {
+                            beginAtZero: true,
+                            max: 11 // UVI Skala von 0 bis 11+
+                        }
+                    }
+                }
+            });
+        })
+.catch((error) => console.error("Fetch-Fehler:", error)); 
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color; // Erzeugt eine zufällige Farbe
+  }// Gibt Fehler im Konsolenlog aus, falls die Daten nicht abgerufen werden können
+});
--- unload.php
+++ unload.php
@@ -4,4 +4,30 @@ require_once 'config.php';
 
 header('Content-Type: application/json');
 
+if (isset($_GET['datetime'])) {
+    $datetime = $_GET['datetime'];
+} else {
+    $datetime = '2025-10-07 11:00:00';
+}
+
+if (empty($datetime)) {
+    echo json_encode(['error' => 'Datetime parameter is required.']);
+    exit;
+}
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+
+    $sql = "SELECT * FROM UV_Index WHERE datetime = ? ORDER BY location DESC";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([$datetime]);
+
+    $results = $stmt->fetchAll();
+
+    echo json_encode($results);
+} catch (PDOException $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
 ?>
